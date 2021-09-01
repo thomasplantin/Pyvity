eacb4b91a15ebceb339e7683509d25e622dee162
thomasplantin <>
2021-08-31 22:35:02 -0500

    pushing all files

---
+++ pyvity.py
@@ -0,0 +1,79 @@
+import pygame
+import math
+
+from settings import Ball, Window
+import constants
+
+def setup():
+    pygame.init()
+    window = Window(constants.WINDOW_WIDTH, constants.WINDOW_HEIGHT, "PYVITY")
+    screen = pygame.display.set_mode(window.dimensions)
+    pygame.display.set_caption(window.title)
+    ball = Ball(color=(255, 244, 125), center=(window.dimensions[0] / 2, window.dimensions[1] - 5), radius=5)
+
+    return screen, ball
+
+def render_window(screen, ball, line, shoot):
+    screen.fill((64, 64, 64))
+    ball.draw(screen)
+    if not shoot:
+        pygame.draw.line(screen, (255, 255, 255), line[0], (line[0][0] + line[1][0], line[0][1] + line[1][1]))
+    pygame.display.update()
+
+def get_vectors(mouse_coordinates, ball_coordinates):
+    vector_x = mouse_coordinates[0] - ball_coordinates[0]
+    vector_y = mouse_coordinates[1] - ball_coordinates[1]
+    power = math.sqrt(math.pow(vector_x, 2) + math.pow(vector_y, 2))
+    angle = math.asin(abs(vector_y / power))
+
+    if power > constants.POWER_LIMIT:
+        power = constants.POWER_LIMIT
+        if vector_x < 0:
+            vector_x = - power * math.cos(angle)
+        else:
+            vector_x = power * math.cos(angle)
+        vector_y = - power * math.sin(angle)
+
+    return (vector_x, vector_y)
+
+def in_motion(ball, start_x_y, vectors, time):
+    if ball.center[1] <= constants.WINDOW_HEIGHT - ball.radius:
+        ball.center = ball.ball_path(start_x_y, vectors, time)
+        return True
+    ball.center = (ball.center[0], constants.WINDOW_HEIGHT - ball.radius)
+    return False
+
+def run(screen, ball):
+    running = True
+    shoot = False
+    time = 0
+    start_x_y = [0, 0]
+    while running:
+
+        if shoot:
+            shoot = in_motion(ball, start_x_y, vectors, time)
+            time += 0.1
+        
+        else:
+            vectors = get_vectors(pygame.mouse.get_pos(), ball.center)
+            line = [ball.center, vectors]
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+            if event.type == pygame.MOUSEBUTTONDOWN and shoot == False:
+                time = 0
+                shoot = True
+                start_x_y = [ball.center[0], ball.center[1]]
+        render_window(screen, ball, line, shoot)
+
+
+def main():
+    screen, ball = setup()
+    run(screen, ball)
+    pygame.quit()
+
+
+if __name__ == "__main__":
+    main()
---
+++ settings.py
@@ -0,0 +1,30 @@
+import pygame
+import math
+import constants
+
+class Ball:
+    def __init__(self, color=(0, 0, 0), center=(400, 250), radius=20):
+        self.color = color
+        self.center = center
+        self.radius = radius
+
+    def draw(self, window):
+        pygame.draw.circle(window, (0, 0, 0), self.center, self.radius)
+        pygame.draw.circle(window, self.color, self.center, self.radius - 1)
+
+    @staticmethod
+    def ball_path(start_x_y, vectors, time):
+        velocity_x = vectors[0] * (1 - constants.PERCENTAGE_HANDICAP)
+        velocity_y = vectors[1] * (1 - constants.PERCENTAGE_HANDICAP)
+        dist_x = velocity_x * time
+        dist_y = velocity_y * time + 0.5 * constants.GRAVITY * math.pow(time, 2)
+        return (round(start_x_y[0] + dist_x), round(start_x_y[1] + dist_y))
+
+        
+
+class Window:
+    def __init__(self, width, height, title="PYVITY"):
+        self.dimensions = (width, height)
+        self.title = title
+
+    
